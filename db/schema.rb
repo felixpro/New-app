0b7248621c29915d69853a7363390501118bd154
Felix Pujols <pujols.fr@gmail.com>
2018-11-08 17:46:58 -0400

    commit 4.7 added cost field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_135304) do
+ActiveRecord::Schema.define(version: 2018_11_07_211834) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_135304) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cost"
   end
 
 end
