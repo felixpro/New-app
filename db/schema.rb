3da69ce29c0327222eab083ba814c35e5242565d
Felix Pujols <pujols.fr@gmail.com>
2018-11-11 10:34:13 -0400

    Commit 4.9  new column added to products named color, color renamed for colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_191853) do
+ActiveRecord::Schema.define(version: 2018_11_11_141434) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_11_10_191853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+    t.string "colour"
   end
 
 end
