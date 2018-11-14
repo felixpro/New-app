bbf8fdbd3f3c4b88c0a2392aaa5d24e448b3fd92
Felix Pujols <pujols.fr@gmail.com>
2018-11-14 12:31:41 -0400

    Commit 5.0  new controller added named as order, the view and show pages were created inside the order view folder. A landing page have been created inside the simple_pages

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,9 @@
 class SimplePagesController < ApplicationController
   def about
   end
+
+  def landing_page
+    @featured_product = Product.first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'config/root'
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  # root 'simple_pages#index'
+  root 'simple_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 end
