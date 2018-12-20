89e07852a9c0e753f8cbd6ff84dfafb0e8495a6d
Felix Pujols <pujols.fr@gmail.com>
2018-12-20 00:23:54 -0400

    7.2 New model added
    The model named as Comment has been added

---
+++ db/migrate/20181220041015_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :description
+      t.integer :review
+      t.integer :user_id
+      t.integer :product_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_155546) do
+ActiveRecord::Schema.define(version: 2018_12_20_041015) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "review"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
