3e66928ea46b7497f03778c1aa2bd8bda30a67b7
Felix Pujols <pujols.fr@gmail.com>
2019-01-10 10:07:13 -0400

    Admin flag added to users
    The admins have the the opportunity to destroy comments.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :read, :all
+    if user.admin?
+      can :manage, :all
+    end
   end
 end
