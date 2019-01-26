4f1da24356f8aca580e95f4396f2c6731e708571
Felix Pujols <pujols.fr@gmail.com>
2019-01-26 15:43:25 -0400

    8.1 Creating payments

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,24 @@
 
 Rails.application.routes.draw do
   resources :orders, only: %i[index show create destroy]
-
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: { registrations: "user_registrations" }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: { registrations: 'user_registrations' }
 
   resources :products do
     resources :comments
   end
+
   resources :users
+  resources :products
 
-    resources :products
-    get 'simple_pages/about'
-    get 'simple_pages/contact'
-    get 'config/root'
-    get 'simple_pages/index'
-    get 'simple_pages/landing_page'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  get 'config/root'
+  get 'simple_pages/index'
+  get 'simple_pages/landing_page'
 
-    root 'simple_pages#index'
+  root 'simple_pages#index'
 
-    post 'simple_pages/thank_you'
-
+  post 'simple_pages/thank_you'
+  post 'payments/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
